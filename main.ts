d53bcf6530feedac850c13ce23986d812d7d94cb
matiss <matissjurevics@gmail.com>
2023-02-28 14:01:34 +0000

    Mostly finished

--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { TranslatePrompt, EditPrompt } from "./modals";
 
+dotenv.config();
 // Remember to rename these classes and interfaces!
+
+const configuration = new Configuration({
+	apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,74 +35,181 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: "aicomp",
+			name: "AI Completion",
+			hotkeys: [{ modifiers: ["Alt", "Shift"], key: "c" }],
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const prompt = editor.getSelection();
+				const context = editor
+					.getRange(
+						{ line: 0, ch: 0 },
+						{ line: editor.getCursor("from").line, ch: 0 }
+					)
+					.trim();
+				let message = `Provided context (which may or may not be relavent): "${context}", Complete the following prompt: "${prompt}"`;
+				new Notice("✒️ Writing...");
+				const completion = await openai.createCompletion({
+					model: "text-davinci-003",
+					prompt: message,
+					temperature: 0.7,
+					max_tokens: 350,
+				});
+
+				editor.replaceSelection(completion.data.choices[0].text);
+				new Notice("Completed! 🚀");
+			},
 		});
 
+		this.addCommand({
+			id: "summarise",
+			name: "Summarise",
+			hotkeys: [{ modifiers: ["Alt", "Shift"], key: "s" }],
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const prompt = editor.getSelection();
+				const context = editor
+					.getRange(
+						{ line: 0, ch: 0 },
+						{ line: editor.getCursor("from").line, ch: 0 }
+					)
+					.trim();
+				let message = `Provided context: (which may or may not be relavent) "${context}", Summarise the following: "${prompt}"`;
+				new Notice("📝 Summarising...");
+
+				const completion = await openai.createCompletion({
+					model: "text-davinci-003",
+					prompt: message,
+					temperature: 0.7,
+					max_tokens: 364,
+				});
+
+				editor.replaceSelection(
+					`## Summary\n\n${completion.data.choices[0].text?.trim()}`
+				);
+
+				new Notice("Summarised! 🚀");
+			},
+		});
+
+		this.addCommand({
+			id: "translate",
+			name: "Translate",
+			hotkeys: [{ modifiers: ["Alt", "Shift"], key: "t" }],
+
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const prompt = editor.getSelection();
+				const context = editor
+					.getRange(
+						{ line: 0, ch: 0 },
+						{ line: editor.getCursor("from").line, ch: 0 }
+					)
+					.trim();
+				let language = "english";
+				
+
+				new TranslatePrompt(this.app, async (result) => {
+					language = result;
+					let message = `Provided context (which may or may not be relavent): "${context}", Translate the following: "${prompt}" into ${language}`;
+					console.log(message);
+					new Notice("📖 Translating...");
+					const completion = await openai.createCompletion({
+						model: "text-davinci-003",
+						prompt: message,
+						temperature: 0.7,
+						max_tokens: 364,
+					});
+					console.log(
+						`completion: ${completion.data.choices[0].text?.trim()}`
+					);
+
+					editor.replaceSelection(
+						completion.data.choices[0].text.trim()
+					);
+				}).open();
+			},
+		});
+
+		this.addCommand({
+			id: "edit",
+			name: "Edit",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const prompt = editor.getSelection();
+				const context = editor
+					.getRange(
+						{ line: 0, ch: 0 },
+						{ line: editor.getCursor("from").line, ch: 0 }
+					)
+					.trim();
+				let message:string;
+				
+				new EditPrompt(this.app, async (result) => {
+					message = `Provided context (which may or may not be relavent): "${context}", Edit the following: "${prompt}" so that the following demand is met: "${result}"`;
+					new Notice("Loading...");
+					const completion = await openai.createCompletion({
+						model: "text-davinci-003",
+						prompt: message,
+						temperature: 0.7,
+						max_tokens: 364,
+					});
+
+					editor.replaceSelection(completion.data.choices[0].text.trim());
+					new Notice("Edited! 🚀");
+				}).open();
+				
+			}
+		})
+		
+		this.addCommand({
+			id: "elaborate",
+			name: "Elaborate",
+			hotkeys: [{ modifiers: ["Alt", "Shift"], key: "e" }],
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				const prompt = editor.getSelection();
+				const context = editor
+					.getRange(
+						{ line: 0, ch: 0 },
+						{ line: editor.getCursor("from").line, ch: 0 }
+					)
+					.trim();
+				let message = `Provided context (which may or may not be relavent): "${context}", Elaborate on the following: "${prompt}"`;
+				new Notice("📝 Elaborating...");
+
+				const completion = await openai.createCompletion({
+					model: "text-davinci-003",
+					prompt: message,
+					temperature: 0.7,
+					max_tokens: 768,
+				});
+
+				editor.replaceSelection(
+					`${completion.data.choices[0].text?.trim()}`
+				);
+
+				new Notice("Elaborated! 🚀");
+			}
+		});
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,21 +217,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -116,22 +228,24 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
